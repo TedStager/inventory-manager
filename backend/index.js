ccf6d0a851ca479952088d76fb414293b6a4cd37
Ted Stager <twstager@gmail.com>
2023-10-10 17:05:00 -0400

    split routes for items and users

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes/routes');
+//const routes = require('./routes/routes');
+
+const userRoutes = require('./routes/user_routes');
+const itemRoutes = require('./routes/item_routes');
+
 require('dotenv').config();
 
 const app = express();
 const mongoString = process.env.DATABASE_URL;
 
 app.use(express.json());
-app.use('/api', routes);
+//app.use('/api', routes);
+app.use('/api', userRoutes);
+app.use('/api', itemRoutes);
 
 app.listen(3000, () => {
 	console.log("Server started at ${3000}");
--- backend/routes/item_routes.js
+++ backend/routes/item_routes.js
@@ -3,29 +3,7 @@ const express = require('express');
 const router = express.Router();
 module.exports = router;
 
-const User = require('../models/user');
 const Item = require('../models/item');
-
-// Add user
-router.post('/users', async (req, res) => {
-	console.log("Request for new user:");
-	console.log(req.body);
-
-	const newUser = new User({
-		username: req.body.username,
-		businessName: req.body.businessName,
-		password: req.body.password,
-	});
-
-	try {
-		const saveResponse = await newUser.save();
-		res.status(200).json(saveResponse);
-	}
-
-	catch (error) {
-		res.status(400).json({message: error.message});
-	}
-});
 
 // Add item
 router.post('/items', async (req, res) => {
