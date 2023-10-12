a01dbdde6b2e94230fdb305c7c736fa3916cbb5e
Ted Stager <twstager@gmail.com>
2023-10-12 17:30:48 -0400

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require ('cors');
 
 const userRoutes = require('./routes/user_routes');
 const itemRoutes = require('./routes/item_routes');
@@ -9,6 +10,7 @@ require('dotenv').config();
 const app = express();
 const mongoString = process.env.DATABASE_URL;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', userRoutes);
 app.use('/api', itemRoutes);
